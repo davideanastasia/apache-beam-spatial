1afad4fe7330a154b1ec05824fd71641c06e5150
Davide Anastasia <davide.anastasia@gmail.com>
2019-09-07 08:54:43 +0100

    Change license to Apache 2.0

--- core/src/main/java/com/davideanastasia/beam/spatial/engine/TiledIntervalWindow.java
+++ core/src/main/java/com/davideanastasia/beam/spatial/engine/TiledIntervalWindow.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.davideanastasia.beam.spatial.engine;
 
 import com.google.common.base.MoreObjects;
@@ -35,25 +52,9 @@ public class TiledIntervalWindow extends BoundedWindow {
         this.end = start.plus(size);
     }
 
-    public String getHexAddr() {
-        return hexAddr;
-    }
-
-    public Instant start() {
-        return start;
-    }
-
-    public Instant end() {
-        return end;
-    }
-
     public Instant maxTimestamp() {
         return end.minus(1L);
     }
-
-//    public boolean contains(TiledIntervalWindow other) {
-//        return this.hexAddr.equals(other.hexAddr) && !this.start.isAfter(other.start) && !this.end.isBefore(other.end);
-//    }
 
     private boolean isDisjoint(TiledIntervalWindow other) {
         return !this.end.isAfter(other.start) || !other.end.isAfter(this.start);
